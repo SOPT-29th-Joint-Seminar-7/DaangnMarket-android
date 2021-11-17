5c78bed9519f06df96b8f7714338da2617097108
Leekangmin <kkk5474096@naver.com>
2021-11-17 20:58:30 +0900

    [chore] asset 추가

--- app/src/main/java/org/sopt/daangnmarketandroid/ui/home/HomeActivity.kt
+++ app/src/main/java/org/sopt/daangnmarketandroid/ui/home/HomeActivity.kt
@@ -1,12 +1,34 @@
 package org.sopt.daangnmarketandroid.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.daangnmarketandroid.R
+import org.sopt.daangnmarketandroid.databinding.ActivityHomeBinding
+import org.sopt.daangnmarketandroid.ui.writing.WritingActivity
 
-class HomeActivity: AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViewEvent()
+        fabClickEvent()
+
+    }
+
+    private fun initViewEvent() {
+        val mainFragment = MainFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_product_list, mainFragment)
+            .commit()
+    }
+
+    private fun fabClickEvent() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, WritingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
