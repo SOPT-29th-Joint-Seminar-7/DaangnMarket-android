afa6f98351ea980ab9f13f57c02a21cb7620b64d
hyein <hyein2072@gmail.com>
2021-11-18 03:46:08 +0900

    [ui]write view 완성

--- app/src/main/java/org/sopt/daangnmarketandroid/ui/writing/WritingActivity.kt
+++ app/src/main/java/org/sopt/daangnmarketandroid/ui/writing/WritingActivity.kt
@@ -3,10 +3,14 @@ package org.sopt.daangnmarketandroid.ui.writing
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.daangnmarketandroid.R
+import org.sopt.daangnmarketandroid.databinding.ActivityWritingBinding
 
 class WritingActivity: AppCompatActivity() {
+    private lateinit var  binding: ActivityWritingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_writing)
+
+        binding = ActivityWritingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
